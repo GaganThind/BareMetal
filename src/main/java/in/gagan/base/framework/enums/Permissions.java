80b820be8bc3a4869691f2a86e5004def97b3b3c
gaganthind <gaganthind@protonmail.com>
2020-10-06 17:27:08 +0530

    Removed redundant code in Permission enum

--- src/main/java/in/gagan/base/framework/enums/Permissions.java
+++ src/main/java/in/gagan/base/framework/enums/Permissions.java
@@ -1,18 +1,8 @@
 package in.gagan.base.framework.enums;
 
 public enum Permissions {
-	ADMIN_WRITE("admin:write"),
-	ADMIN_READ("admin:read"),
-	USER_WRITE("user:write"),
-	USER_READ("user:read");
-	
-	private final String permission;
-	
-	Permissions(String permission) {
-		this.permission = permission;
-	}
-	
-	public String getPermission() {
-		return this.permission;
-	}
+	ADMIN_WRITE,
+	ADMIN_READ,
+	USER_WRITE,
+	USER_READ;
 }
