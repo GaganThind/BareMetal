06b39adf0829a59d1298cfc2688566d447780432
gaganthind <gaganthind@protonmail.com>
2020-10-18 20:20:59 +0530

    Add Dummy user on server startup

--- src/main/java/in/gagan/base/framework/component/CommandLineAppStartupRunner.java
+++ src/main/java/in/gagan/base/framework/component/CommandLineAppStartupRunner.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import in.gagan.base.framework.service.DummyDataService;
 import in.gagan.base.framework.service.UserRegisterationService;
 
 @Component
@@ -11,14 +12,18 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
 	
 	private final UserRegisterationService userRegistrationSvc;
 	
+	private final DummyDataService dummyDataSvc;
+	
 	@Autowired
-	public CommandLineAppStartupRunner(UserRegisterationService userRegistrationSvc) {
+	public CommandLineAppStartupRunner(UserRegisterationService userRegistrationSvc, DummyDataService dummyDataSvc) {
 		this.userRegistrationSvc = userRegistrationSvc;
+		this.dummyDataSvc = dummyDataSvc;
 	}
 	
 	@Override
 	public void run(String... args) throws Exception {
 		this.userRegistrationSvc.createAdminUser();
+		this.dummyDataSvc.createDummyData();
 	}
 
 }
