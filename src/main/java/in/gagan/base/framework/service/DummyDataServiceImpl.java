012776ba0a487b66306cc4a20b4ce5d4ccaac427
gaganthind <gaganthind@protonmail.com>
2020-11-03 11:15:25 +0530

    Enable the dummy users by default

--- src/main/java/in/gagan/base/framework/service/DummyDataServiceImpl.java
+++ src/main/java/in/gagan/base/framework/service/DummyDataServiceImpl.java
@@ -10,6 +10,7 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import in.gagan.base.framework.constant.ApplicationConstants;
 import in.gagan.base.framework.entity.Role;
 import in.gagan.base.framework.entity.User;
 import in.gagan.base.framework.enums.Gender;
@@ -41,18 +42,19 @@ public class DummyDataServiceImpl implements DummyDataService {
 	public void createDummyData() {
 		User user = new User("A", "B", "Test1@e.com", null, Gender.M, "T");
 		user.addRole(new Role(ADMIN));
-		
+		user.setActiveSw(ApplicationConstants.CHAR_Y);
 		
 		User user1 = new User("A1", "B1", "Test2@e.com", null, Gender.F, "T");
 		user1.addRole(new Role(ADMIN_BASIC));
-		
+		user1.setActiveSw(ApplicationConstants.CHAR_Y);
 		
 		User user2 = new User("A2", "B2", "Test3@e.com", null, Gender.F, "T");
 		user2.addRole(new Role(USER));
-		
+		user2.setActiveSw(ApplicationConstants.CHAR_Y);
 		
 		User user3 = new User("A3", "B3", "Test4@e.com", null, Gender.M, "T");
 		user3.addRole(new Role(USER_BASIC));
+		user3.setActiveSw(ApplicationConstants.CHAR_Y);
 		
 		this.userDataSvc.saveUser(user);
 		this.verificationTokenSvc.generateTokenForUser(user);
