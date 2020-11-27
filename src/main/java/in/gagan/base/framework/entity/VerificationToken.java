3ad84aa4e2c3ad649fb22a20108554b6d9f033bf
gaganthind <gaganthind@protonmail.com>
2020-11-27 19:59:00 +0530

    Only delete the verificationtoken from db and not the related user

--- src/main/java/in/gagan/base/framework/entity/VerificationToken.java
+++ src/main/java/in/gagan/base/framework/entity/VerificationToken.java
@@ -36,7 +36,7 @@ public class VerificationToken extends Auditable implements BaseEntity {
 	@Column(name="TOKEN", nullable = false, unique = true)
 	private String token;
 	
-	@OneToOne(cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
+	@OneToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
 	@JoinColumn(name="USER_ID", nullable = false)
 	private User user;
 	
