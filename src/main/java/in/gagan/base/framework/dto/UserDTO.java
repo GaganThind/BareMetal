e67e6a42744fd164320b1c9a0462106fc05445a5
gaganthind <gaganthind@protonmail.com>
2020-12-08 16:48:22 +0530

    Initialize both password and Matching password with password if using UserDTO param Constructor

--- src/main/java/in/gagan/base/framework/dto/UserDTO.java
+++ src/main/java/in/gagan/base/framework/dto/UserDTO.java
@@ -72,7 +72,7 @@ public class UserDTO implements Serializable {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
-		this.password = password;
+		this.password = this.matchingPassword = password;
 		this.dob = dob;
 		this.gender = gender;
 		this.userRole = userRole;
