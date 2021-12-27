98c0461d0838de27d4982ae25ca7333815fa6d70
gaganthind <gaganthind@protonmail.com>
2021-12-27 13:14:15 +0530

    Added preAuth for full country search

--- src/main/java/in/gagan/base/framework/controller/AddressController.java
+++ src/main/java/in/gagan/base/framework/controller/AddressController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,6 +45,7 @@ public class AddressController extends AbstractController {
 	 * 
 	 * @return countries - data consisting of all the countries
 	 */
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@GetMapping(value = "/country", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Set<CountryDTO>> getCountries() {
 		Optional<Set<CountryDTO>> countryDTOs = this.addressSvc.getCountries();
