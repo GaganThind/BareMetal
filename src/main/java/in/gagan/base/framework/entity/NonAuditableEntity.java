bcbec087c4780a425a951fb996d4e4c7257ed6ba
gaganthind <gaganthind@protonmail.com>
2022-01-01 12:26:18 +0530

    Create non auditable base entity

---
+++ src/main/java/in/gagan/base/framework/entity/NonAuditableEntity.java
@@ -0,0 +1,35 @@
+package in.gagan.base.framework.entity;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * This class enables auditing of persistence data.
+ * 
+ * @author gaganthind
+ *
+ */
+@MappedSuperclass
+public class NonAuditableEntity implements BaseEntity {
+	
+	public NonAuditableEntity() {  }
+	
+	public NonAuditableEntity(char activeSw) {
+		super();
+		this.activeSw = activeSw;
+	}
+	
+	@Column(name="ACTIVE_SW", nullable = false, length = 1)
+	private char activeSw;
+	
+	@Override
+	public char isActive() {
+		return activeSw;
+	}
+
+	@Override
+	public void setActiveSw(char activeSw) {
+		this.activeSw = activeSw;
+	}
+
+}
