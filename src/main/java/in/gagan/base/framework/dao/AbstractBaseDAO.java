9da3f4be23c6cb21ac3946359cf92c3b665eac8d
gaganthind <gaganthind@protonmail.com>
2022-03-12 15:09:30 +0530

    Unused Imports

--- src/main/java/in/gagan/base/framework/dao/AbstractBaseDAO.java
+++ src/main/java/in/gagan/base/framework/dao/AbstractBaseDAO.java
@@ -23,7 +23,6 @@ import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
