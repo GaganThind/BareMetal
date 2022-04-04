67950f894b921c1c672f43ea6782347bbc8370a6
gaganthind <gaganthind@protonmail.com>
2022-04-04 14:29:25 +0530

    Remove unnecessary serial version code

--- src/main/java/in/gagan/base/framework/exception/UsernameExistException.java
+++ src/main/java/in/gagan/base/framework/exception/UsernameExistException.java
@@ -24,14 +24,8 @@ package in.gagan.base.framework.exception;
  *
  */
 public final class UsernameExistException extends Exception {
-
-	/**
-	 * Serial Version
-	 */
-	private static final long serialVersionUID = -7880518881602624383L;
 	
 	public UsernameExistException(String id) {
 		super(new StringBuilder("User with Username/Email_id: ").append(id).append(" already exists").toString());
 	}
-	
 }
